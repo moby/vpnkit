a44f6d6eb42cec2f1d6d19fccaf651d7ca983a5a
David Scott <dave.scott@docker.com>
2016-02-11 09:38:11 +0000

    com.docker.slirp: use the Active_list.Instance signature
    Rather than repeating it inline.
    
    Signed-off-by: David Scott <dave.scott@docker.com>

--- v1/cmd/com.docker.slirp/src/forward.mli
+++ v1/cmd/com.docker.slirp/src/forward.mli
@@ -1,22 +1,3 @@
 
-type t
-(** The client requests one of these *)
-
-val to_string: t -> string
-val of_string: string -> (t, [ `Msg of string ]) Result.result
-
-type context = Tcpip_stack.t
-(** The context in which a [t] is [start]ed, for example a TCP/IP stack *)
-
-val start: context -> t -> (t, [ `Msg of string ]) Result.result Lwt.t
-
-val stop: t -> unit Lwt.t
-
-type key
-(** Some unique primary key *)
-
-module Map: Map.S with type key = key
-
-val get_key: t -> key
-
-val description_of_format: string
+include Active_list.Instance
+  with type context = Tcpip_stack.t
