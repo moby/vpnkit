bb715f7b5243b785a79053c580cb245f6aa57411
David Scott <dave@recoil.org>
2016-05-01 17:27:34 +0100

    hostnet: add more interface documentation
    This is a start at documenting the main entrypoint.
    
    Signed-off-by: David Scott <dave.scott@docker.com>

---
+++ v1/hostnet/lib/slirp.mli
@@ -0,0 +1,35 @@
+(*
+ * Copyright (C) 2016 David Scott <dave.scott@docker.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ *)
+
+type pcap = (string * int64 option) option
+(** Packet capture configuration. None means don't capture; Some (file, limit)
+    means write pcap-formatted data to file. If the limit is None then the
+    file will grow without bound; otherwise the file will be closed when it is
+    bigger than the given limit. *)
+
+module Make(Vmnet: Sig.VMNET)(Resolv_conv: Sig.RESOLV_CONF): sig
+  val connect: Vmnet.t -> Ipaddr.V4.t -> Ipaddr.V4.t -> unit Lwt.t
+  (** [connect vmnet peer_ip local_ip] starts a slirp TCP/IP stack on the ethernet
+      connection [vmnet}], where the local host has IP [local_ip] and the peer has
+      IP [peer_ip]. *)
+end
+
+val print_pcap: pcap -> string
+
+val client_macaddr: Macaddr.t
+
+val server_macaddr: Macaddr.t
