cb17ea1330f98b15e80f65587ca17e37abaea45c
Gaetan de Villele <gdevillele@gmail.com>
2016-05-03 11:41:53 -0700

    Updated ocaml code related to vsock sockets
    Signed-off-by: Gaetan de Villele <gdevillele@gmail.com>

--- v1/cmd/com.docker.slirp/src/main.ml
+++ v1/cmd/com.docker.slirp/src/main.ml
@@ -104,22 +104,25 @@ let main socket port_control vsock_path db debug = Lwt_main.run @@ main_t socket
 open Cmdliner
 
 (* NOTE(aduermael): it seems to me that "/var/tmp/com.docker.slirp.socket" is a default value, right?
-the db socket path is now dynamic, depending on current user's home directory. Could we just
+This socket path is now dynamic, depending on current user's home directory. Could we just
 make it fail instead? In case no argument is supplied? *)
 let socket =
   Arg.(value & opt string "/var/tmp/com.docker.slirp.socket" & info [ "socket" ] ~docv:"SOCKET")
 
 (* NOTE(aduermael): it seems to me that "/var/tmp/com.docker.port.socket" is a default value, right?
-the db socket path is now dynamic, depending on current user's home directory. Could we just
+This socket path is now dynamic, depending on current user's home directory. Could we just
 make it fail instead? In case no argument is supplied? *)
 let port_control_path =
   Arg.(value & opt string "/var/tmp/com.docker.port.socket" & info [ "port-control" ] ~docv:"PORT")
 
+(* NOTE(aduermael): it seems to me that "/var/tmp/com.docker.vsock/connect" is a default value, right?
+This socket path is now dynamic, depending on current user's home directory. Could we just
+make it fail instead? In case no argument is supplied? *)
 let vsock_path =
   Arg.(value & opt string "/var/tmp/com.docker.vsock/connect" & info [ "vsock-path" ] ~docv:"VSOCK")
 
 (* NOTE(aduermael): it seems to me that "/var/tmp/com.docker.db.socket" is a default value, right?
-the db socket path is now dynamic, depending on current user's home directory. Could we just
+This socket path is now dynamic, depending on current user's home directory. Could we just
 make it fail instead? In case no argument is supplied? *)
 let db_path =
   Arg.(value & opt string "/var/tmp/com.docker.db.socket" & info [ "db" ] ~docv:"DB")
