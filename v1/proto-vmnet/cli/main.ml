7d50138ee9048248e2eb943bc39890738cd3d487
Anil Madhavapeddy <anil@recoil.org>
2016-05-12 09:51:18 +0100

    Initial import of proto-vmnet library

---
+++ v1/proto-vmnet/cli/main.ml
@@ -0,0 +1,121 @@
+(*
+ * Copyright (C) 2016 David Scott <dave.scott@docker.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ *)
+open Lwt
+
+let src =
+  let src = Logs.Src.create "vmnet" ~doc:"vmnet CLI" in
+  Logs.Src.set_level src (Some Logs.Debug);
+  src
+
+module Log = (val Logs.src_log src : Logs.LOG)
+
+let or_failwith = function
+  | `Error (`Msg x) -> failwith x
+  | `Ok x -> x
+
+let connect_client socket debug =
+  let s = Lwt_unix.socket Lwt_unix.PF_UNIX Lwt_unix.SOCK_STREAM 0 in
+  Lwt_unix.connect s (Unix.ADDR_UNIX socket)
+  >>= fun () ->
+  Vmnet_client.of_fd s
+  >>= fun r ->
+  let c = or_failwith r in
+  Lwt.return c
+
+let bind_main_t socket ip port stream debug =
+  let ip = Ipaddr.V4.of_string_exn ip in
+  connect_client socket debug
+  >>= fun c ->
+  Vmnet_client.bind_ipv4 c (ip, port, stream)
+  >>= fun r ->
+  let fd = or_failwith r in
+  Log.debug (fun f -> f "Received fd successfully");
+  Lwt_unix.listen fd 5;
+  Lwt_unix.accept fd
+  >>= fun (client, _) ->
+  Log.debug (fun f -> f "Connected to client");
+  Lwt_unix.write client "hello\n" 0 6
+  >>= fun _ ->
+  Lwt_unix.close fd
+
+let bind_main socket ip port stream debug = Lwt_main.run @@ bind_main_t socket ip port stream debug
+
+let uninstall_main_t socket debug =
+  connect_client socket debug
+  >>= fun c ->
+  Vmnet_client.uninstall c
+  >>= fun r ->
+  let () = or_failwith r in
+  Log.debug (fun f -> f "uninstall ok");
+  Lwt.return ()
+
+let uninstall_main socket debug = Lwt_main.run @@ uninstall_main_t socket debug
+
+open Cmdliner
+
+let socket =
+  Arg.(value & opt string "/var/tmp/com.docker.vmnetd.socket" & info [ "socket" ] ~docv:"SOCKET")
+
+let ip =
+  Arg.(value & opt string "0.0.0.0" & info [ "ip" ] ~docv:"IP")
+
+let port =
+  Arg.(value & opt int 80 & info [ "port" ] ~docv:"PORT")
+
+let stream =
+  let doc = "Bind a SOCK_STREAM, else a SOCK_DGRAM" in
+  Arg.(value & flag & info [ "stream" ] ~doc)
+
+let debug =
+  let doc = "Verbose debug logging to stdout" in
+  Arg.(value & flag & info [ "debug" ] ~doc)
+
+let bind_cmd =
+  let doc = "talk to vmnetd" in
+  let man =
+    [`S "DESCRIPTION";
+     `P "Ask vmnetd to bind a socket"]
+  in
+  Term.(pure bind_main $ socket $ ip $ port $ stream $ debug),
+  Term.info "bind" ~doc ~man
+
+let uninstall_cmd =
+  let doc = "uninstall vmnetd" in
+  let man =
+    [`S "DESCRIPTION";
+     `P "Ask vmnetd to self-uninstall"]
+  in
+  Term.(pure uninstall_main $ socket $ debug),
+  Term.info "uninstall" ~doc ~man
+
+let help = [
+ `S "MORE HELP";
+ `P "Use `$(mname) $(i,COMMAND) --help' for help on a single command."; `Noblank;
+ `S "BUGS"; `P (Printf.sprintf "Check bug reports at docker/pinata");
+]
+
+let default_cmd =
+  let doc = "Sends simple commands to vmnetd" in
+  let man = help in
+  Term.(ret (pure (`Help (`Pager, None)))),
+  Term.info "vmnet-cli" ~version:"0.1" ~doc ~man
+
+let () =
+  Printexc.record_backtrace true;
+  match Term.eval_choice default_cmd [ bind_cmd; uninstall_cmd ] with
+  | `Error _ -> exit 1
+  | _ -> exit 0
---
+++ v1/proto-vmnet/lib/vmnet.ml
@@ -0,0 +1,516 @@
+(*
+ * Copyright (C) 2016 David Scott <dave.scott@docker.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ *)
+open Lwt
+open Sexplib.Std
+
+let src =
+  let src = Logs.Src.create "vmnet" ~doc:"vmnet" in
+  Logs.Src.set_level src (Some Logs.Info);
+  src
+
+module Log = (val Logs.src_log src : Logs.LOG)
+
+let log_exception_continue description f =
+  Lwt.catch
+    (fun () -> f ())
+    (fun e ->
+       Log.err (fun f -> f "%s: caught %s" description (Printexc.to_string e));
+       Lwt.return ()
+    )
+
+let default_mtu = 1500
+
+let ethernet_header_length = 14 (* no VLAN *)
+
+type stats = {
+  mutable rx_bytes: int64;
+  mutable rx_pkts: int32;
+  mutable tx_bytes: int64;
+  mutable tx_pkts: int32;
+}
+
+type t = {
+  mutable fd: Lwt_unix.file_descr option;
+  stats: stats;
+  client_macaddr: Macaddr.t;
+  server_macaddr: Macaddr.t;
+  read_header: Cstruct.t;
+  write_header: Cstruct.t;
+  write_m: Lwt_mutex.t;
+  mutable pcap: Lwt_unix.file_descr option;
+  mutable pcap_size_limit: int64 option;
+  pcap_m: Lwt_mutex.t;
+  mutable listeners: (Cstruct.t -> unit Lwt.t) list;
+  mutable listening: bool;
+}
+
+module Init = struct
+
+  cstruct msg {
+      uint8_t magic[5];   (* VMN3T *)
+      uint32_t version;   (* 1 *)
+      uint8_t commit[40];
+    } as little_endian
+
+  type t = {
+    magic: string;
+    version: int32;
+    commit: string;
+  } with sexp
+
+  let to_string t = Sexplib.Sexp.to_string (sexp_of_t t)
+
+  let sizeof = sizeof_msg
+
+  let default = {
+    magic = "VMN3T";
+    version = 1l;
+    commit = "0123456789012345678901234567890123456789";
+  }
+
+  let marshal t rest =
+    set_msg_magic t.magic 0 rest;
+    set_msg_version rest t.version;
+    set_msg_commit t.commit 0 rest;
+    Cstruct.shift rest sizeof_msg
+
+  let unmarshal rest =
+    let magic = Cstruct.to_string @@ get_msg_magic rest in
+    let version = get_msg_version rest in
+    let commit = Cstruct.to_string @@ get_msg_commit rest in
+    let rest = Cstruct.shift rest sizeof_msg in
+    `Ok ({ magic; version; commit }, rest)
+end
+
+module Command = struct
+
+  cstruct msg {
+      uint8_t command;
+    } as little_endian
+
+  type t =
+    | Ethernet of string (* 36 bytes *)
+  with sexp
+
+  let to_string t = Sexplib.Sexp.to_string (sexp_of_t t)
+
+  let sizeof = sizeof_msg + 36
+
+  let marshal t rest = match t with
+    | Ethernet uuid ->
+      set_msg_command rest 1;
+      let rest = Cstruct.shift rest sizeof_msg in
+      Cstruct.blit_from_string uuid 0 rest 0 (String.length uuid);
+      Cstruct.shift rest (String.length uuid)
+
+  let unmarshal rest =
+    match get_msg_command rest with
+    | 1 ->
+      let uuid = Cstruct.(to_string (sub rest 1 36)) in
+      let rest = Cstruct.shift rest 37 in
+      `Ok (Ethernet uuid, rest)
+    | n -> `Error (`Msg (Printf.sprintf "Unknown command: %d" n))
+end
+
+module Vif = struct
+  cstruct msg {
+      uint16_t mtu;
+      uint16_t max_packet_size;
+      uint8_t macaddr[6];
+    } as little_endian
+
+  type t = {
+    mtu: int;
+    max_packet_size: int;
+    client_macaddr: Macaddr.t;
+  } with sexp
+
+  let to_string t = Sexplib.Sexp.to_string (sexp_of_t t)
+
+  let create client_macaddr () =
+    let mtu = default_mtu in
+    let max_packet_size = 1550 in
+    { mtu; max_packet_size; client_macaddr }
+
+  let sizeof = sizeof_msg
+
+  let marshal t rest =
+    set_msg_mtu rest t.mtu;
+    set_msg_max_packet_size rest t.max_packet_size;
+    set_msg_macaddr (Macaddr.to_bytes t.client_macaddr) 0 rest;
+    Cstruct.shift rest sizeof_msg
+
+  let unmarshal rest =
+    let mtu = get_msg_mtu rest in
+    let max_packet_size = get_msg_max_packet_size rest in
+    try
+      let client_macaddr = Macaddr.of_bytes_exn @@ Cstruct.to_string @@ get_msg_macaddr rest in
+      `Ok ({ mtu; max_packet_size; client_macaddr }, Cstruct.shift rest sizeof_msg)
+    with _ ->
+      `Error (`Msg (Printf.sprintf "Failed to parse MAC: [%s]" (Cstruct.to_string @@ get_msg_macaddr rest)))
+
+end
+
+module Packet = struct
+  cstruct msg {
+      uint16_t len;
+    } as little_endian
+
+  type t = int
+
+  let sizeof = sizeof_msg
+
+  let marshal t rest =
+    set_msg_len rest t
+
+  let unmarshal rest =
+    let t = get_msg_len rest in
+    `Ok (t, Cstruct.shift rest sizeof)
+end
+
+module Infix = struct
+  let ( >>= ) m f = m >>= function
+    | `Ok x -> f x
+    | `Error x -> Lwt.return (`Error x)
+end
+
+let error_of_failure f = Lwt.catch f (fun e -> Lwt.return (`Error (`Msg (Printexc.to_string e))))
+
+let get_fd t = match t.fd with
+  | Some fd -> fd
+  | None -> failwith "Vmnet connection is disconnected"
+
+let server_negotiate t =
+  error_of_failure
+    (fun () ->
+      let fd = get_fd t in
+      let buf = Cstruct.create Init.sizeof in
+      Lwt_cstruct.(complete (read fd) buf)
+      >>= fun () ->
+      let open Infix in
+      Lwt.return (Init.unmarshal buf)
+      >>= fun (init, _) ->
+      Log.info (fun f -> f "PPP.negotiate: received %s" (Init.to_string init));
+      let (_: Cstruct.t) = Init.marshal Init.default buf in
+      let open Lwt.Infix in
+      Lwt_cstruct.(complete (write fd) buf)
+      >>= fun () ->
+      let buf = Cstruct.create Command.sizeof in
+      Lwt_cstruct.(complete (read fd) buf)
+      >>= fun () ->
+      let open Infix in
+      Lwt.return (Command.unmarshal buf)
+      >>= fun (command, _) ->
+      Log.info (fun f -> f "PPP.negotiate: received %s" (Command.to_string command));
+      let vif = Vif.create t.client_macaddr () in
+      let buf = Cstruct.create Vif.sizeof in
+      let (_: Cstruct.t) = Vif.marshal vif buf in
+      let open Lwt.Infix in
+      Log.info (fun f -> f "PPP.negotiate: sending %s" (Vif.to_string vif));
+      Lwt_cstruct.(complete (write fd) buf)
+      >>= fun () ->
+      Lwt.return (`Ok ())
+    )
+
+let client_negotiate t =
+  error_of_failure
+    (fun () ->
+      let open Lwt.Infix in
+      let fd = get_fd t in
+      let buf = Cstruct.create Init.sizeof in
+      let (_: Cstruct.t) = Init.marshal Init.default buf in
+      Lwt_cstruct.(complete (write fd) buf)
+      >>= fun () ->
+      Lwt_cstruct.(complete (read fd) buf)
+      >>= fun () ->
+      let open Infix in
+      Lwt.return (Init.unmarshal buf)
+      >>= fun (init, _) ->
+      Log.info (fun f -> f "Client.negotiate: received %s" (Init.to_string init));
+      let buf = Cstruct.create Command.sizeof in
+      let uuid = String.make 36 'X' in
+      let (_: Cstruct.t) = Command.marshal (Command.Ethernet uuid) buf in
+      let open Lwt.Infix in
+      Lwt_cstruct.(complete (write fd) buf)
+      >>= fun () ->
+      let buf = Cstruct.create Vif.sizeof in
+      Lwt_cstruct.(complete (read fd) buf)
+      >>= fun () ->
+      let open Infix in
+      Lwt.return (Vif.unmarshal buf)
+      >>= fun (vif, _) ->
+      Log.info (fun f -> f "Client.negotiate: vif %s" (Vif.to_string vif));
+      Lwt.return (`Ok ())
+    )
+
+let start_capture t ?size_limit filename =
+  Lwt_mutex.with_lock t.pcap_m
+    (fun () ->
+      ( match t.pcap with
+        | Some fd ->
+          Lwt_unix.close fd
+        | None ->
+          Lwt.return ()
+      ) >>= fun () ->
+      Lwt_unix.openfile filename [ Unix.O_WRONLY; Unix.O_TRUNC; Unix.O_CREAT ] 0o0644
+      >>= fun fd ->
+      let buf = Cstruct.create Pcap.LE.sizeof_pcap_header in
+      let open Pcap in
+      let open Pcap.LE in
+      set_pcap_header_magic_number buf Pcap.magic_number;
+      set_pcap_header_version_major buf Pcap.major_version;
+      set_pcap_header_version_minor buf Pcap.minor_version;
+      set_pcap_header_thiszone buf 0l;
+      set_pcap_header_sigfigs buf 4l;
+      set_pcap_header_snaplen buf 1500l;
+      set_pcap_header_network buf (Pcap.Network.to_int32 Pcap.Network.Ethernet);
+      Lwt_cstruct.(complete (write fd) buf)
+      >>= fun () ->
+      t.pcap <- Some fd;
+      t.pcap_size_limit <- size_limit;
+      Lwt.return ()
+    )
+
+let stop_capture_already_locked t = match t.pcap with
+  | None -> Lwt.return ()
+  | Some fd ->
+    Lwt_unix.close fd
+    >>= fun () ->
+    t.pcap <- None;
+    t.pcap_size_limit <- None;
+    Lwt.return ()
+
+let stop_capture t =
+  Lwt_mutex.with_lock t.pcap_m
+    (fun () ->
+      stop_capture_already_locked t
+    )
+
+let make ~client_macaddr ~server_macaddr fd =
+  let fd = Some fd in
+  let stats = { rx_bytes = 0L; rx_pkts = 0l; tx_bytes = 0L; tx_pkts = 0l } in
+  let read_header = Cstruct.create Packet.sizeof in
+  let write_header = Cstruct.create Packet.sizeof in
+  let write_m = Lwt_mutex.create () in
+  let pcap = None in
+  let pcap_size_limit = None in
+  let pcap_m = Lwt_mutex.create () in
+  let listeners = [] in
+  let listening = false in
+  { fd; stats; client_macaddr; server_macaddr; read_header; write_header; write_m; pcap; pcap_size_limit; pcap_m; listeners; listening }
+
+let of_fd ~client_macaddr ~server_macaddr fd =
+  let open Infix in
+  let t = make ~client_macaddr ~server_macaddr fd in
+  server_negotiate t
+  >>= fun () ->
+  Lwt.return (`Ok t)
+
+let client_of_fd ~client_macaddr ~server_macaddr fd =
+  let open Infix in
+  let t = make ~client_macaddr ~server_macaddr fd in
+  client_negotiate t
+  >>= fun () ->
+  Lwt.return (`Ok t)
+
+let disconnect t = match t.fd with
+  | None -> Lwt.return ()
+  | Some fd ->
+    t.fd <- None;
+    Log.info (fun f -> f "Vmnet.disconnect closing fd");
+    Lwt_unix.close fd
+
+let capture t bufs =
+  match t.pcap with
+  | None -> Lwt.return ()
+  | Some pcap ->
+    Lwt_mutex.with_lock t.pcap_m
+      (fun () ->
+         let len = List.(fold_left (+) 0 (map Cstruct.len bufs)) in
+         let time = Unix.gettimeofday () in
+         let secs = Int32.of_float time in
+         let usecs = Int32.of_float (1e6 *. (time -. (floor time))) in
+         let buf = Cstruct.create Pcap.sizeof_pcap_packet in
+         let open Pcap in
+         let open Pcap.LE in
+         set_pcap_packet_ts_sec buf secs;
+         set_pcap_packet_ts_usec buf usecs;
+         set_pcap_packet_incl_len buf @@ Int32.of_int len;
+         set_pcap_packet_orig_len buf @@ Int32.of_int len;
+         Lwt_cstruct.(complete (write pcap) buf)
+         >>= fun () ->
+         let rec loop = function
+           | [] -> Lwt.return ()
+           | buf :: bufs ->
+             Lwt_cstruct.(complete (write pcap) buf)
+             >>= fun () ->
+             loop bufs in
+         loop bufs
+         >>= fun () ->
+         match t.pcap_size_limit with
+         | None -> Lwt.return () (* no limit *)
+         | Some limit ->
+           let limit = Int64.(sub limit (of_int len)) in
+           t.pcap_size_limit <- Some limit;
+           if limit < 0L
+           then stop_capture_already_locked t
+           else Lwt.return ()
+      )
+
+let drop_on_error f = Lwt.catch f (fun _ -> Lwt.return ())
+
+let write t buf =
+  Lwt_mutex.with_lock t.write_m
+    (fun () ->
+      drop_on_error
+        (fun () ->
+           capture t [ buf ]
+           >>= fun () ->
+           let len = Cstruct.len buf in
+           if len > (default_mtu + ethernet_header_length) then begin
+             Log.err (fun f ->
+               f "Dropping over-large ethernet frame, length = %d, mtu = %d" len default_mtu
+             );
+             Lwt.return_unit
+           end else begin
+             Packet.marshal len t.write_header;
+             let fd = get_fd t in
+             Lwt_cstruct.(complete (write fd) t.write_header)
+             >>= fun () ->
+             Log.debug (fun f ->
+                 let b = Buffer.create 128 in
+                 Cstruct.hexdump_to_buffer b buf;
+                 f "sending\n%s" (Buffer.contents b)
+               );
+             Lwt_cstruct.(complete (write fd) buf)
+           end
+        )
+    )
+
+let writev t bufs =
+  Lwt_mutex.with_lock t.write_m
+    (fun () ->
+      drop_on_error
+        (fun () ->
+           capture t bufs
+           >>= fun () ->
+           let len = List.(fold_left (+) 0 (map Cstruct.len bufs)) in
+           if len > (default_mtu + ethernet_header_length) then begin
+             Log.err (fun f ->
+               f "Dropping over-large ethernet frame, length = %d, mtu = %d" len default_mtu
+             );
+             Lwt.return_unit
+           end else begin
+             Packet.marshal len t.write_header;
+             let fd = get_fd t in
+             Lwt_cstruct.(complete (write fd) t.write_header)
+             >>= fun () ->
+             Log.debug (fun f ->
+                 let b = Buffer.create 128 in
+                 List.iter (Cstruct.hexdump_to_buffer b) bufs;
+                 f "sending\n%s" (Buffer.contents b)
+               );
+             let rec loop = function
+               | [] -> Lwt.return ()
+               | buf :: bufs ->
+                 Lwt_cstruct.(complete (write fd) buf)
+                 >>= fun () ->
+                 loop bufs in
+             loop bufs
+           end
+         )
+    )
+
+let listen t callback =
+  if t.listening then begin
+    Log.info (fun f -> f "PPP.listen: called a second time: doing nothing");
+    Lwt.return ();
+  end else begin
+    t.listening <- true;
+    let rec loop () =
+      let open Infix in
+      Lwt.catch
+        (fun () ->
+           let open Lwt.Infix in
+           let fd = get_fd t in
+           Lwt_cstruct.(complete (read fd) t.read_header)
+           >>= fun () ->
+           let open Infix in
+           Lwt.return (Packet.unmarshal t.read_header)
+           >>= fun (len, _) ->
+           let buf = Cstruct.create len in
+           let open Lwt.Infix in
+           Lwt_cstruct.(complete (read fd) buf)
+           >>= fun () ->
+           capture t [ buf ]
+           >>= fun () ->
+           Log.debug (fun f ->
+               let b = Buffer.create 128 in
+               Cstruct.hexdump_to_buffer b buf;
+               f "received\n%s" (Buffer.contents b)
+             );
+           let callback buf = log_exception_continue "PPP.listen callback" (fun () -> callback buf) in
+           Lwt.async (fun () -> callback buf);
+           List.iter (fun callback -> Lwt.async (fun () -> callback buf)) t.listeners;
+           Lwt.return (`Ok true)
+        ) (function
+            | End_of_file ->
+              Log.info (fun f -> f "PPP.listen: closing connection");
+              Lwt.return (`Ok false);
+            | e ->
+              Log.err (fun f -> f "PPP.listen: caught unexpected %s: disconnecting" (Printexc.to_string e));
+              let open Lwt.Infix in
+              disconnect t
+              >>= fun () ->
+              Lwt.return (`Ok false)
+          )
+      >>= fun continue ->
+      if continue then loop () else Lwt.return (`Ok ()) in
+    Lwt.async @@ loop;
+    Lwt.return ();
+  end
+
+let add_listener t callback =
+  t.listeners <- callback :: t.listeners
+
+let mac t = t.server_macaddr
+
+type page_aligned_buffer = Io_page.t
+
+type buffer = Cstruct.t
+
+type error = [
+  | `Unknown of string
+  | `Unimplemented
+  | `Disconnected
+]
+
+type macaddr = Macaddr.t
+
+type 'a io = 'a Lwt.t
+
+type id = unit
+
+let get_stats_counters t = t.stats
+
+let reset_stats_counters t =
+  t.stats.rx_bytes <- 0L;
+  t.stats.tx_bytes <- 0L;
+  t.stats.rx_pkts <- 0l;
+  t.stats.tx_pkts <- 0l
+
+let get_id _ = ()
