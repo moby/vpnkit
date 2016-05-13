22748ac065494918f6de230edf3ab18b94153d80
David Scott <dave@recoil.org>
2016-05-13 01:34:04 -0700

    Add missing files conn_lwt_unix.ml and conn_lwt_unix.mli
    Signed-off-by: David Scott <dave.scott@docker.com>

---
+++ v1/hostnet/lib/conn_lwt_unix.ml
@@ -0,0 +1,25 @@
+(*
+ * Copyright (C) 2015 David Scott <dave.scott@unikernel.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ *)
+
+open Lwt.Infix
+
+type fd = Lwt_unix.file_descr
+let connect fd = fd
+let close = Lwt_unix.close
+
+let read fd buf = Lwt_cstruct.(complete (read fd) buf)
+let write fd buf = Lwt_cstruct.(complete (write fd) buf)
