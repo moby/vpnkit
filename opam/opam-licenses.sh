fb218b31c664e5d6ebc9986edeb2b70ed882f7f9
Thomas Gazagnaire <thomas@gazagnaire.org>
2016-06-08 09:48:50 +0100

    win: make opam-licenses.sh quoting more robust on windows
    hint: try to listen to shellcheck advices
    
    Signed-off-by: Thomas Gazagnaire <thomas@gazagnaire.org>

--- opam/opam-licenses.sh
+++ opam/opam-licenses.sh
@@ -2,6 +2,7 @@
 set -e
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
+OPAM_ROOT=${REPO_ROOT}/_build/opam
 
 # Collect the license metadata in a central place
 OUTPUT=${REPO_ROOT}/opam/licenses/
@@ -12,86 +13,86 @@ if [ $# = 0 ]; then
   echo "  -- look up license terms for a list of packages"
 fi
 
-for DEP in $*; do
-  echo Calculating transitive dependencies required by $DEP
-  opam list --required-by $DEP --recursive --installed | tail +2 > dependency.$DEP.raw
-  cat dependency.$DEP.raw | awk '{print $1"."$2}' > dependency.$DEP
-  rm dependency.$DEP.raw
+for DEP in "$@"; do
+  echo "Calculating transitive dependencies required by $DEP"
+  opam list --required-by "$DEP" --recursive --installed | tail -n +2 > "dependency.$DEP.raw"
+  awk '{print $1"."$2}' < "dependency.$DEP.raw" > "dependency.$DEP"
+  rm "dependency.$DEP.raw"
 done
 cat dependency.* | sort | uniq > all-packages.txt
-rm -f dependency.*
+rm -f "dependency.*"
 
-rm -f *.extracted
-for PACKAGE in $(cat all-packages.txt); do
-  echo looking for license for ${PACKAGE}
-  URL=$(opam info --field upstream-url $PACKAGE)
-  rm -f /tmp/opam.out
+rm -f "*.extracted"
+while read -r PACKAGE; do
+  echo "looking for license for ${PACKAGE}"
+  URL=$(opam info --field upstream-url "$PACKAGE")
+  rm -f "/tmp/opam.out"
   if [ -z "${URL}" ]; then
-    echo $PACKAGE has no source: skipping
+    echo "$PACKAGE has no source: skipping"
     continue
   fi
-  if [ -e $OUTPUT/LICENSE.$PACKAGE.skip ]; then
-    echo $PACKAGE is not linked: skipping
+  if [ -e "$OUTPUT/LICENSE.$PACKAGE.skip" ]; then
+    echo "$PACKAGE is not linked: skipping"
     continue
   fi
   # if a file has a manually-edited override, use it
-  if [ ! -e $OUTPUT/LICENSE.$PACKAGE ]; then
-    rm -f $PACKAGE.files
-    KIND=$(opam info --field upstream-kind $PACKAGE)
-    if [ $KIND = git ] || [ $KIND = local ]; then
+  if [ ! -e "$OUTPUT/LICENSE.$PACKAGE" ]; then
+    rm -f "$PACKAGE.files"
+    KIND=$(opam info --field upstream-kind "$PACKAGE")
+    if [ "$KIND" = git ] || [ "$KIND" = local ]; then
       # there is an entry under the system switch packages.dev if pinned
-      PROJECT=$(echo $PACKAGE | sed -n 's/^\([^.]*\).*/\1/p')
-      if [ -e $OPAMROOT/system/packages.dev/$PROJECT ]; then
-        DIR=$OPAMROOT/system/packages.dev/$PROJECT
+      PROJECT=$(echo "$PACKAGE" | sed -n 's/^\([^.]*\).*/\1/p')
+      if [ -e "$OPAM_ROOT/system/packages.dev/$PROJECT" ]; then
+        DIR=$OPAM_ROOT/system/packages.dev/$PROJECT
       else
-        DIR=$OPAMROOT/packages.dev/$PACKAGE
+        DIR=$OPAM_ROOT/packages.dev/$PACKAGE
       fi
-      echo $DIR
-      ls $DIR | grep LICENSE >> $PACKAGE.files || true
-      ls $DIR | grep COPYING >> $PACKAGE.files || true
-      if [ -z "$(cat $PACKAGE.files)" ]; then
+      echo "$DIR"
+      ls "$DIR" | grep LICENSE >> "$PACKAGE.files" || true
+      ls "$DIR" | grep COPYING >> "$PACKAGE.files" || true
+      if [ -z "$(cat "$PACKAGE.files")" ]; then
         echo "No LICENSE or COPYING file found in $DIR; please write LICENSE.$PACKAGE yourself"
         exit 1
       fi
       # extract all the license files we found
-      rm -f $OUTPUT/LICENSE.$PACKAGE.extracted
-      xargs -I % cp $DIR/% . < $PACKAGE.files
-      for i in $(cat $PACKAGE.files); do
-        echo "$i:" >> $OUTPUT/LICENSE.$PACKAGE.extracted
-        cat $i >> $OUTPUT/LICENSE.$PACKAGE.extracted
-        rm $i
-      done
-      rm -f $PACKAGE.files
+      rm -f "$OUTPUT/LICENSE.$PACKAGE.extracted"
+      xargs -I % cp "$DIR"/% . < "$PACKAGE.files"
+      while read -r i; do
+        echo "$i:" >> "$OUTPUT/LICENSE.$PACKAGE.extracted"
+        cat "$i" >> "$OUTPUT/LICENSE.$PACKAGE.extracted"
+        rm "$i"
+      done < "$PACKAGE.files"
+      rm -f "$PACKAGE.files"
     else
-      FILE=$OPAMROOT/packages.dev/$PACKAGE/$(basename $URL)
-      if [ ! -e $FILE ]; then
-        echo $FILE doesnt exist
+      FILE=$OPAM_ROOT/packages.dev/$PACKAGE/$(basename "$URL")
+      if [ ! -e "$FILE" ]; then
+        echo "$FILE doesnt exist"
         exit 1
       fi
-      echo $FILE
-      rm -f $PACKAGE.files
+      echo "$FILE"
+      rm -f "$PACKAGE.files"
       # NB lwt's LICENSE file references the COPYING file
-      tar -tf $FILE | grep LICENSE >> $PACKAGE.files || true
-      tar -tf $FILE | grep COPYING >> $PACKAGE.files || true
-      if [ -z "$(cat $PACKAGE.files)" ]; then
+      tar -tf "$FILE" | grep LICENSE >> "$PACKAGE.files" || true
+      tar -tf "$FILE" | grep COPYING >> "$PACKAGE.files" || true
+      if [ -z "$(cat "$PACKAGE.files")" ]; then
         echo "No LICENSE or COPYING file found in $FILE; please write LICENSE.$PACKAGE yourself"
         exit 1
       fi
       # extract all the license files we found
-      rm -f $OUTPUT/LICENSE.$PACKAGE.extracted
-      tar -xf $FILE -T $PACKAGE.files
-      for i in $(cat $PACKAGE.files); do
-        echo "$i:" >> $OUTPUT/LICENSE.$PACKAGE.extracted
-        cat $i >> $OUTPUT/LICENSE.$PACKAGE.extracted
-        rm $i
-      done
-      for i in $(sort -r $PACKAGE.files); do
-        DIR=$(dirname $i)
-        if [ -e $DIR ]; then
-          rmdir -p $DIR
+      rm -f "$OUTPUT/LICENSE.$PACKAGE.extracted"
+      tar -xf "$FILE" -T "$PACKAGE.files"
+      while read -r i; do
+        echo "$i:" >> "$OUTPUT/LICENSE.$PACKAGE.extracted"
+        cat "$i" >> "$OUTPUT/LICENSE.$PACKAGE.extracted"
+        rm "$i"
+      done < "$PACKAGE.files"
+      for i in $(sort -r "$PACKAGE.files"); do
+        DIR=$(dirname "$i")
+        if [ -e "$DIR" ]; then
+          rmdir -p "$DIR"
         fi
       done
-      rm -f $PACKAGE.files
+      rm -f "$PACKAGE.files"
     fi
   fi
-done
+done < all-packages.txt
