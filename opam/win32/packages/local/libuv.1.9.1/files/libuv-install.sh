ef4b137d55a6ee7419d0b4c2ba501680fd092bd2
David Scott <dave@recoil.org>
2016-06-28 10:31:25 -0700

    opam: add a libuv package to build from source
    This is needed because libuv is not in cygwin.
    
    Signed-off-by: David Scott <dave.scott@docker.com>

---
+++ opam/win32/packages/local/libuv.1.9.1/files/libuv-install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -ex
+
+# use the default pkg-config directory for the .pc
+PKG_CONFIG_PATH=$(pkg-config --variable pc_path pkg-config | cut -f 1 -d ":")
+
+# put the library and headers in /usr/local
+PREFIX=/usr/local
+
+LIBDIR="$PREFIX/lib/"
+INCLUDEDIR="$PREFIX/include/libuv"
+
+mkdir -p "$LIBDIR"
+cp libuv.a "$LIBDIR"
+mkdir -p "$INCLUDEDIR"
+cp include/*.h "$INCLUDEDIR"
+mkdir -p "$PKG_CONFIG_PATH"
+cp libuv.pc "$PKG_CONFIG_PATH"
