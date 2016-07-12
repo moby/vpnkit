0b413a5c54bce7c4d401b885665adf053684e020
Thomas Gazagnaire <thomas@gazagnaire.org>
2016-07-12 11:32:19 +0100

    Add a `--version` command-line argument
    Fix #56
    
    Signed-off-by: Thomas Gazagnaire <thomas@gazagnaire.org>

--- src/com.docker.slirp/pkg/pkg.ml
+++ src/com.docker.slirp/pkg/pkg.ml
@@ -3,7 +3,10 @@
 #require "topkg"
 
 open Topkg
+open Result
+
+let distrib = Pkg.distrib ~files_to_watermark:(fun () -> Ok [ "src/main.ml" ]) ()
 
 let () =
-  Pkg.describe ~change_logs:[] ~metas:[] "com.docker.slirp" @@ fun c ->
+  Pkg.describe ~distrib ~change_logs:[] ~metas:[] "com.docker.slirp" @@ fun c ->
   Ok [ Pkg.bin "src/main" ~dst:"com.docker.slirp" ]
