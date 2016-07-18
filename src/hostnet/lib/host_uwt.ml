b3a86dc0e2e4a75ea536146f1fb5e27ccd5cfc84
Ian Campbell <ian.campbell@docker.com>
2016-07-18 15:24:11 +0100

    Use String.concat in preference to multiple ^'s
    Signed-off-by: Ian Campbell <ian.campbell@docker.com>

--- src/hostnet/lib/host_uwt.ml
+++ src/hostnet/lib/host_uwt.ml
@@ -86,7 +86,7 @@ module Sockets = struct
       (if Hashtbl.mem table (src, src_port, dst, dst_port) then begin
           Lwt.return (Some (Hashtbl.find table (src, src_port, dst, dst_port)))
         end else begin
-         let description = Ipaddr.to_string src ^ ":" ^ (string_of_int src_port) ^ "-" ^ (Ipaddr.to_string dst) ^ ":" ^ (string_of_int dst_port) in
+         let description = String.concat "" [Ipaddr.to_string src; ":"; string_of_int src_port; "-"; Ipaddr.to_string dst; ":"; string_of_int dst_port] in
          if Ipaddr.compare dst Ipaddr.(V4 V4.broadcast) = 0 then begin
            Log.debug (fun f -> f "Socket.Datagram.input %s: ignoring broadcast packet" description);
            Lwt.return None
