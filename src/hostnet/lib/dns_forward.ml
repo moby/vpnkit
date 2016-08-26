4c891ec3ac68e923dce8812096455b0f1fa20524
David Scott <dave@recoil.org>
2016-08-26 14:28:28 +0100

    DNS: serve entries read from /etc/hosts
    When we receive an A or AAAA DNS request we first look in our cache
    of /etc/hosts entries. If it's not in our cache then forward it to
    the upstream server as normal.
    
    This allows users to edit /etc/hosts on the Mac/Windows host and then
    resolve these names from containers.
    
    Signed-off-by: David Scott <dave.scott@docker.com>

--- src/hostnet/lib/dns_forward.ml
+++ src/hostnet/lib/dns_forward.ml
@@ -37,6 +37,40 @@ let choose_server ~nth all =
   then None
   else Some (string_of_int nth ^ suffix, List.nth (only_ipv4 all) (nth mod l))
 
+let lookup_locally = function
+  | _, Some request ->
+    let open Dns.Packet in
+    begin match request with
+    | { id; detail; additionals; questions = [ { q_class = Q_IN; q_type = Q_A; q_name; _ } ]; _ } ->
+      begin match List.fold_left (fun found (name, ip) -> match found, ip with
+        | Some v4, _           -> Some v4
+        | None,   Ipaddr.V4 v4 ->
+          if Dns.Name.to_string q_name = name then Some v4 else None
+        | None,   Ipaddr.V6 _  -> None
+      ) None !(Hosts.etc_hosts) with
+      | None -> None
+      | Some v4 ->
+        Log.debug (fun f -> f "DNS[%04x] %s is %s in in /etc/hosts" id (Dns.Name.to_string q_name) (Ipaddr.V4.to_string v4));
+        let answers = [ { name = q_name; cls = RR_IN; flush = false; ttl = 0l; rdata = A v4 } ] in
+        Some { Dns.Packet.id; detail; questions = request.questions; authorities=[]; additionals; answers }
+      end
+    | { id; detail; additionals; questions = [ { q_class = Q_IN; q_type = Q_AAAA; q_name; _ } ]; _ } ->
+      begin match List.fold_left (fun found (name, ip) -> match found, ip with
+        | Some v6, _           -> Some v6
+        | None,   Ipaddr.V6 v6 ->
+          if Dns.Name.to_string q_name = name then Some v6 else None
+        | None,   Ipaddr.V4 _  -> None
+      ) None !(Hosts.etc_hosts) with
+      | None -> None
+      | Some v6 ->
+        Log.debug (fun f -> f "DNS[%04x] %s is %s in in /etc/hosts" id (Dns.Name.to_string q_name) (Ipaddr.V6.to_string v6));
+        let answers = [ { name = q_name; cls = RR_IN; flush = false; ttl = 0l; rdata = AAAA v6 } ] in
+        Some { Dns.Packet.id; detail; questions = request.questions; authorities=[]; additionals; answers }
+      end
+    | _ -> None
+    end
+  | _, _ -> None
+
 module Make(Ip: V1_LWT.IPV4) (Udp:V1_LWT.UDPV4) (Resolv_conf: Sig.RESOLV_CONF) (Socket: Sig.SOCKETS) (Time: V1_LWT.TIME) = struct
 
 let input ~nth ~udp ~src ~dst ~src_port buf =
@@ -45,19 +79,36 @@ let input ~nth ~udp ~src ~dst ~src_port buf =
 
   let dns = parse_dns buf in
 
-  Log.debug (fun f -> f "DNS[%s] %s:%d -> %s %s" (tidstr_of_dns dns) src_str src_port dst_str (string_of_dns dns));
+  let reply buffer =
+    Log.debug (fun f -> f "DNS[%s] %s:%d <- %s (%s)" (tidstr_of_dns dns) src_str src_port dst_str (string_of_dns (parse_dns buffer)));
+    Udp.write ~source_port:53 ~dest_ip:src ~dest_port:src_port udp buffer in
 
-  Resolv_conf.get ()
-  >>= fun all ->
-  match choose_server ~nth all.Resolver.resolvers with
-  | Some (dst_str, (dst, dst_port)) ->
-    Log.debug (fun f -> f "DNS[%s] Forwarding to %s (%s)" (tidstr_of_dns dns) (Ipaddr.to_string dst) dst_str);
-    let reply buffer =
-      Log.debug (fun f -> f "DNS[%s] %s:%d <- %s (%s)" (tidstr_of_dns dns) src_str src_port dst_str (string_of_dns (parse_dns buffer)));
-      Udp.write ~source_port:53 ~dest_ip:src ~dest_port:src_port udp buffer in
-    let userdesc = "DNS[" ^ (tidstr_of_dns dns) ^ "]" in
-    Socket.Datagram.input ~userdesc ~reply ~src:(Ipaddr.V4 src, src_port) ~dst:(dst, dst_port) ~payload:buf ()
+  Log.debug (fun f -> f "DNS[%s] %s:%d -> %s %s" (tidstr_of_dns dns) src_str src_port dst_str (string_of_dns dns));
+  (* Is this an A or AAAA query which can be satisfied from /etc/hosts? *)
+  match lookup_locally dns with
+  | Some response ->
+    let obuf = Dns.Buf.create 4096 in
+    begin match Dns.Protocol.Server.marshal obuf response response with
+    | None ->
+      Log.err (fun f -> f "DNS[%s] failed to marshal response" (tidstr_of_dns dns));
+      Lwt.return_unit
+    | Some buf ->
+      let buf = Cstruct.of_bigarray buf in
+      reply buf
+    end
   | None ->
-    Log.err (fun f -> f "DNS[%s] No upstream DNS server configured: dropping request" (tidstr_of_dns dns));
-    Lwt.return_unit
+    Resolv_conf.get ()
+    >>= fun all ->
+    begin match choose_server ~nth all.Resolver.resolvers with
+    | Some (dst_str, (dst, dst_port)) ->
+      Log.debug (fun f -> f "DNS[%s] Forwarding to %s (%s)" (tidstr_of_dns dns) (Ipaddr.to_string dst) dst_str);
+      let reply buffer =
+        Log.debug (fun f -> f "DNS[%s] %s:%d <- %s (%s)" (tidstr_of_dns dns) src_str src_port dst_str (string_of_dns (parse_dns buffer)));
+        Udp.write ~source_port:53 ~dest_ip:src ~dest_port:src_port udp buffer in
+      let userdesc = "DNS[" ^ (tidstr_of_dns dns) ^ "]" in
+      Socket.Datagram.input ~userdesc ~reply ~src:(Ipaddr.V4 src, src_port) ~dst:(dst, dst_port) ~payload:buf ()
+    | None ->
+      Log.err (fun f -> f "DNS[%s] No upstream DNS server configured: dropping request" (tidstr_of_dns dns));
+      Lwt.return_unit
+    end
 end
