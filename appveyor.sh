ed5071ccecb42f12e19bfcbde882c5b9646bdf62
Magnus Skjegstad <magnus@skjegstad.com>
2016-09-12 14:51:35 +0100

    Make curl less silent
    Signed-off-by: Magnus Skjegstad <magnus@skjegstad.com>

--- appveyor.sh
+++ appveyor.sh
@@ -18,7 +18,7 @@ fi
 export OPAM_LINT="false"
 export OPAMYES=1
 
-curl -fsSL -o "${OPAM_ARCH}.tar.xz" "${OPAM_URL}"
+curl -fSL -o "${OPAM_ARCH}.tar.xz" "${OPAM_URL}"
 tar -xf "${OPAM_ARCH}.tar.xz"
 "${OPAM_ARCH}/install.sh"
 
