647f59275065553926f983256b52932629dd9df7
Magnus Skjegstad <magnus@skjegstad.com>
2016-09-12 15:12:56 +0100

    More debugging... try to use appveyor cli to download
    Signed-off-by: Magnus Skjegstad <magnus@skjegstad.com>

--- appveyor.sh
+++ appveyor.sh
@@ -18,8 +18,8 @@ fi
 export OPAM_LINT="false"
 export OPAMYES=1
 
-curl --help
-curl -SL -o "${OPAM_ARCH}.tar.xz" "${OPAM_URL}"
+echo $PATH
+appveyor DownloadFile "${OPAM_URL}" -FileName "${OPAM_ARCH}.tar.xz"
 tar -xf "${OPAM_ARCH}.tar.xz"
 "${OPAM_ARCH}/install.sh"
 
