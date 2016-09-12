7e1fd993573f0b40c500e8b56d6e3dd4e8ab34c3
Magnus Skjegstad <magnus@skjegstad.com>
2016-09-12 14:54:52 +0100

    Remove -f
    Signed-off-by: Magnus Skjegstad <magnus@skjegstad.com>

--- appveyor.sh
+++ appveyor.sh
@@ -18,7 +18,7 @@ fi
 export OPAM_LINT="false"
 export OPAMYES=1
 
-curl -fSL -o "${OPAM_ARCH}.tar.xz" "${OPAM_URL}"
+curl -SL -o "${OPAM_ARCH}.tar.xz" "${OPAM_URL}"
 tar -xf "${OPAM_ARCH}.tar.xz"
 "${OPAM_ARCH}/install.sh"
 
