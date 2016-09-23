f74ca277776836655904f512bc4fd98944e78c6d
David Scott <dave@recoil.org>
2016-09-23 10:12:45 +0100

    Attempt to fix watermarking
    We substitute %%VERSION%% with the $(git rev-parse HEAD), and attempt
    to substitute the %%*_PINNED%% properly.
    
    Signed-off-by: David Scott <dave.scott@docker.com>

--- src/com.docker.slirp/pkg/pkg.ml
+++ src/com.docker.slirp/pkg/pkg.ml
@@ -5,8 +5,6 @@
 open Topkg
 open Result
 
-let distrib = Pkg.distrib ~files_to_watermark:(fun () -> Ok [ "src/depends.ml" ]) ()
-
 let () =
-  Pkg.describe ~distrib ~change_logs:[] ~metas:[] "com.docker.slirp" @@ fun c ->
+  Pkg.describe ~change_logs:[] ~metas:[] "com.docker.slirp" @@ fun c ->
   Ok [ Pkg.bin "src/main" ~dst:"com.docker.slirp" ]
