83edc6ac59bbe70539d075a49cd564cbbe916893
David Scott <dave@recoil.org>
2016-11-01 16:06:46 +0000

    Wrap an exception handler around Frame.parse
    This ensures that any mis-handling of a mal-formed packet is caught
    locally and returned into a simple `Error (`Msg _)`. Note that it's
    still better to catch the errors locally since then we can create
    more interesting error messages.
    
    Signed-off-by: David Scott <dave.scott@docker.com>

--- src/hostnet/frame.ml
+++ src/hostnet/frame.ml
@@ -15,65 +15,67 @@ let ( >>= ) m f = match m with
   | Error x -> Error x
 
 let parse bufs =
-  if Cstructs.len bufs < 14
-  then Error (`Msg "too short to be an ethernet frame")
-  else begin
-    let ethertype  = Cstructs.BE.get_uint16 bufs 12 in
-    let dst_option = Cstructs.sub bufs 0 6 |> Cstructs.to_string |> Macaddr.of_bytes in
-    let src_option = Cstructs.sub bufs 6 6 |> Cstructs.to_string |> Macaddr.of_bytes in
-    match dst_option, src_option with
-    | None, _ -> Error (`Msg "failed to parse ethernet destination MAC")
-    | _, None -> Error (`Msg "failed to parse ethernet source MAC")
-    | Some src, Some dst ->
-      let inner = Cstructs.shift bufs 14 in
-      ( match ethertype with
-        | 0x0800 ->
-          let vihl  = Cstructs.get_uint8     inner 0 in
-          let len   = Cstructs.BE.get_uint16 inner (1 + 1) in
-          let off   = Cstructs.BE.get_uint16 inner (1 + 1 + 2 + 2) in
-          let proto = Cstructs.get_uint8     inner (1 + 1 + 2 + 2 + 2 + 1) in
-          let src   = Cstructs.BE.get_uint32 inner (1 + 1 + 2 + 2 + 2 + 1 + 1 + 2) |> Ipaddr.V4.of_int32 in
-          let dst   = Cstructs.BE.get_uint32 inner (1 + 1 + 2 + 2 + 2 + 1 + 1 + 2 + 4) |> Ipaddr.V4.of_int32 in
-          let dnf = ((off lsr 8) land 0x40) <> 0 in
-          let ihl = vihl land 0xf in
-          let raw = Cstructs.to_cstruct inner in
-          let inner = Cstructs.sub inner (4 * ihl) (len - 4 * ihl) in
-          ( match proto with
-            | 1 ->
-              let _ty     = Cstructs.get_uint8     inner 0 in
-              let _code   = Cstructs.get_uint8     inner 1 in
-              let _csum   = Cstructs.BE.get_uint16 inner 2 in
-              let _id     = Cstructs.BE.get_uint16 inner 4 in
-              let _seq    = Cstructs.BE.get_uint16 inner 6 in
-              let payload = Cstructs.shift         inner 8 |> Cstructs.to_cstruct in
-              Ok (Icmp { raw; payload = Payload payload })
-            | 6 ->
-              let src     = Cstructs.BE.get_uint16 inner 0 in
-              let dst     = Cstructs.BE.get_uint16 inner 2 in
-              let offres  = Cstructs.get_uint8     inner (2 + 2 + 4 + 4) in
-              let flags   = Cstructs.get_uint8     inner (2 + 2 + 4 + 4 + 1) in
-              let payload = Cstructs.shift         inner ((offres lsr 4) * 4) |> Cstructs.to_cstruct in
-              let syn = (flags land (1 lsl 1)) > 0 in
-              Ok (Tcp { src; dst; syn; raw = Cstructs.to_cstruct inner; payload = Payload payload })
-            | 17 ->
-              let src     = Cstructs.BE.get_uint16 inner 0 in
-              let dst     = Cstructs.BE.get_uint16 inner 2 in
-              let len     = Cstructs.BE.get_uint16 inner 4 in
-              let payload = Cstructs.shift         inner 8 |> Cstructs.to_cstruct in
-              let len = len - 8 in (* subtract header length *)
-              Ok (Udp { src; dst; len; payload = Payload payload })
-            | _ ->
-              Error (`Msg (Printf.sprintf "unknown IPv4 protocol type %d" proto)) )
-          >>= fun payload ->
-          Ok (Ipv4 { src; dst; dnf; ihl; raw; payload })
-        | 0x0806 ->
-          let code    = Cstructs.BE.get_uint16 inner 6 in
-          let op = match code with 1 -> `Request | 2 -> `Reply | _ -> `Unknown in
-          Ok (Arp { op })
-        | _ ->
-          (* This is going to be quite common e.g. with IPv6 *)
-          Ok Unknown )
-      >>= fun payload ->
-      Ok (Ethernet { src; dst; payload })
-
-  end
+  try
+    if Cstructs.len bufs < 14
+    then Error (`Msg "too short to be an ethernet frame")
+    else begin
+      let ethertype  = Cstructs.BE.get_uint16 bufs 12 in
+      let dst_option = Cstructs.sub bufs 0 6 |> Cstructs.to_string |> Macaddr.of_bytes in
+      let src_option = Cstructs.sub bufs 6 6 |> Cstructs.to_string |> Macaddr.of_bytes in
+      match dst_option, src_option with
+      | None, _ -> Error (`Msg "failed to parse ethernet destination MAC")
+      | _, None -> Error (`Msg "failed to parse ethernet source MAC")
+      | Some src, Some dst ->
+        let inner = Cstructs.shift bufs 14 in
+        ( match ethertype with
+          | 0x0800 ->
+            let vihl  = Cstructs.get_uint8     inner 0 in
+            let len   = Cstructs.BE.get_uint16 inner (1 + 1) in
+            let off   = Cstructs.BE.get_uint16 inner (1 + 1 + 2 + 2) in
+            let proto = Cstructs.get_uint8     inner (1 + 1 + 2 + 2 + 2 + 1) in
+            let src   = Cstructs.BE.get_uint32 inner (1 + 1 + 2 + 2 + 2 + 1 + 1 + 2) |> Ipaddr.V4.of_int32 in
+            let dst   = Cstructs.BE.get_uint32 inner (1 + 1 + 2 + 2 + 2 + 1 + 1 + 2 + 4) |> Ipaddr.V4.of_int32 in
+            let dnf = ((off lsr 8) land 0x40) <> 0 in
+            let ihl = vihl land 0xf in
+            let raw = Cstructs.to_cstruct inner in
+            let inner = Cstructs.sub inner (4 * ihl) (len - 4 * ihl) in
+            ( match proto with
+              | 1 ->
+                let _ty     = Cstructs.get_uint8     inner 0 in
+                let _code   = Cstructs.get_uint8     inner 1 in
+                let _csum   = Cstructs.BE.get_uint16 inner 2 in
+                let _id     = Cstructs.BE.get_uint16 inner 4 in
+                let _seq    = Cstructs.BE.get_uint16 inner 6 in
+                let payload = Cstructs.shift         inner 8 |> Cstructs.to_cstruct in
+                Ok (Icmp { raw; payload = Payload payload })
+              | 6 ->
+                let src     = Cstructs.BE.get_uint16 inner 0 in
+                let dst     = Cstructs.BE.get_uint16 inner 2 in
+                let offres  = Cstructs.get_uint8     inner (2 + 2 + 4 + 4) in
+                let flags   = Cstructs.get_uint8     inner (2 + 2 + 4 + 4 + 1) in
+                let payload = Cstructs.shift         inner ((offres lsr 4) * 4) |> Cstructs.to_cstruct in
+                let syn = (flags land (1 lsl 1)) > 0 in
+                Ok (Tcp { src; dst; syn; raw = Cstructs.to_cstruct inner; payload = Payload payload })
+              | 17 ->
+                let src     = Cstructs.BE.get_uint16 inner 0 in
+                let dst     = Cstructs.BE.get_uint16 inner 2 in
+                let len     = Cstructs.BE.get_uint16 inner 4 in
+                let payload = Cstructs.shift         inner 8 |> Cstructs.to_cstruct in
+                let len = len - 8 in (* subtract header length *)
+                Ok (Udp { src; dst; len; payload = Payload payload })
+              | _ ->
+                Error (`Msg (Printf.sprintf "unknown IPv4 protocol type %d" proto)) )
+            >>= fun payload ->
+            Ok (Ipv4 { src; dst; dnf; ihl; raw; payload })
+          | 0x0806 ->
+            let code    = Cstructs.BE.get_uint16 inner 6 in
+            let op = match code with 1 -> `Request | 2 -> `Reply | _ -> `Unknown in
+            Ok (Arp { op })
+          | _ ->
+            (* This is going to be quite common e.g. with IPv6 *)
+            Ok Unknown )
+        >>= fun payload ->
+        Ok (Ethernet { src; dst; payload })
+    end
+  with e ->
+    Error (`Msg ("Failed to parse ethernet frame: " ^ (Printexc.to_string e)))
