5d7e98905f291b331b2566d00e55c7548548db4d
Thomas Gazagnaire <thomas@gazagnaire.org>
2017-07-23 19:46:19 +0200

    bin: cut long lines
    Signed-off-by: Thomas Gazagnaire <thomas@gazagnaire.org>

--- src/bin/connect.ml
+++ src/bin/connect.ml
@@ -1,5 +1,7 @@
 let src =
-  let src = Logs.Src.create "port forward" ~doc:"forward local ports to the VM" in
+  let src =
+    Logs.Src.create "port forward" ~doc:"forward local ports to the VM"
+  in
   Logs.Src.set_level src (Some Logs.Info);
   src
 
@@ -13,39 +15,39 @@ let vsock_port = 62373l
 
 module Make_unix(Host: Sig.HOST) = struct
 
-  let vsock_path = ref (home / "Library/Containers/com.docker.docker/Data/@connect")
+  let vsock_path =
+    ref (home / "Library/Containers/com.docker.docker/Data/@connect")
 
   include Host.Sockets.Stream.Unix
 
   let connect () =
-    connect (!vsock_path)
-    >>= function
+    connect (!vsock_path) >>= function
     | Error (`Msg msg) ->
       Log.err (fun f -> f "vsock connect write got %s" msg);
-      Lwt.fail (Failure msg)
+      Lwt.fail_with msg
     | Ok flow ->
-      let address = Cstruct.of_string (Printf.sprintf "00000003.%08lx\n" vsock_port) in
-      write flow address
-      >>= function
+      let address =
+        Cstruct.of_string (Printf.sprintf "00000003.%08lx\n" vsock_port)
+      in
+      write flow address >>= function
+      | `Ok () -> Lwt.return flow
       | `Eof ->
         Log.err (fun f -> f "vsock connect write got Eof");
-        close flow
-        >>= fun () ->
+        close flow >>= fun () ->
         Lwt.fail End_of_file
       | `Error e ->
         let msg = error_message e in
         Log.err (fun f -> f "vsock connect write got %s" msg);
-        close flow
-        >>= fun () ->
-        Lwt.fail (Failure msg)
-      | `Ok () ->
-        Lwt.return flow
+        close flow >>= fun () ->
+        Lwt.fail_with msg
 end
 
 module Make_hvsock(Host: Sig.HOST) = struct
   (* Avoid using `detach` because we don't want to exhaust the
      thread pool since this will block the main TCP/IP stack. *)
-  module F = Flow_lwt_hvsock_shutdown.Make(Host.Time)(Lwt_hvsock_main_thread.Make(Host.Main))
+  module F =
+    Flow_lwt_hvsock_shutdown.Make(Host.Time)
+      (Lwt_hvsock_main_thread.Make(Host.Main))
 
   type flow = {
     idx: int;
@@ -64,17 +66,16 @@ module Make_hvsock(Host: Sig.HOST) = struct
 
   let connect () = match !hvsockaddr with
   | None ->
-    Log.err (fun f -> f "Please set a Hyper-V socket address for port forwarding");
+    Log.err (fun f ->
+        f "Please set a Hyper-V socket address for port forwarding");
     failwith "Hyper-V socket forwarding not initialised"
   | Some sockaddr ->
     let description = "hvsock" in
-    Host.Sockets.register_connection description
-    >>= fun idx ->
+    Host.Sockets.register_connection description >>= fun idx ->
     let fd = F.Hvsock.create () in
-    F.Hvsock.connect fd sockaddr
-    >>= fun () ->
+    F.Hvsock.connect fd sockaddr >|= fun () ->
     let flow = F.connect fd in
-    Lwt.return { idx; flow }
+    { idx; flow }
 
   let read_into t = F.read_into t.flow
   let read t = F.read t.flow
