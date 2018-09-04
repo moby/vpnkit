d071494ff41061da7c53d6ffbd68138b624df16a
David Scott <dave@recoil.org>
2018-09-04 21:31:32 +0100

    vpnkit-{tap,9pmount}-vsock: log to stderr
    Previously we would log to stdout.
    
    Signed-off-by: David Scott <dave.scott@docker.com>

--- c/vpnkit-9pmount-vsock/log.h
+++ c/vpnkit-9pmount-vsock/log.h
@@ -4,30 +4,26 @@
 extern int verbose;
 #define ERROR(...)                                                      \
     do {                                                                \
-		printf(__VA_ARGS__);                                            \
-        printf("\n");                                                   \
-		fflush(stdout);                                                 \
+        fprintf(stderr, __VA_ARGS__);                                   \
+        fprintf(stderr, "\n");                                          \
     } while (0)
 #define INFO(...)                                                       \
     do {                                                                \
-        printf(__VA_ARGS__);                                            \
-        printf("\n");                                                   \
-        fflush(stdout);                                                 \
+        fprintf(stderr, __VA_ARGS__);                                   \
+        fprintf(stderr, "\n");                                          \
     } while (0)
 #define DBG(...)                                                        \
     do {                                                                \
         if (verbose > 1) {                                              \
-            printf(__VA_ARGS__);                                        \
-            printf("\n");                                               \
-            fflush(stdout);                                             \
+            fprintf(stderr, __VA_ARGS__);                               \
+            fprintf(stderr, "\n");                                      \
         }                                                               \
     } while (0)
 #define TRC(...)                                                        \
     do {                                                                \
         if (verbose > 2) {                                              \
-            printf(__VA_ARGS__);                                        \
-            printf("\n");                                               \
-            fflush(stdout);                                             \
+            fprintf(stderr, __VA_ARGS__);                               \
+            fprintf(stderr, "\n");                                      \
         }                                                               \
     } while (0)
 
