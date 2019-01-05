ee1fc41c9d1d15539f30b62044ba9d9f41a7c26b
David Scott <dave@recoil.org>
2019-01-05 10:05:02 +0000

    appveyor: update to opam2
    Signed-off-by: David Scott <dave.scott@docker.com>

--- scripts/depends.sh
+++ scripts/depends.sh
@@ -10,12 +10,12 @@ case "$(uname -s)" in
 
     # default setttings
     SWITCH="${OPAM_COMP}"
-    OPAM_URL='https://dl.dropboxusercontent.com/s/b2q2vjau7if1c1b/opam64.tar.xz'
+    OPAM_URL='https://github.com/fdopen/opam-repository-mingw/releases/download/0.0.0.2/opam64.tar.xz'
     OPAM_ARCH=opam64
 
     if [ "$PROCESSOR_ARCHITECTURE" != "AMD64" ] && \
            [ "$PROCESSOR_ARCHITEW6432" != "AMD64" ]; then
-        OPAM_URL='https://dl.dropboxusercontent.com/s/eo4igttab8ipyle/opam32.tar.xz'
+	OPAM_URL='https://github.com/fdopen/opam-repository-mingw/releases/download/0.0.0.2/opam32.tar.xz'
         OPAM_ARCH=opam32
     fi
 
