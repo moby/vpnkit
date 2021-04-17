b45f64a9009a6ee7566fc63d73b673ee3cf41f28
David Scott <dave@recoil.org>
2021-04-17 11:56:10 +0100

    Simplify "quiet" logging type
    The new format looks like:
    
    ```
    time="2021-04-17T10:56:51Z" level=info msg="usernet: Increasing preemptive thread pool size to 1024 threads"
    ```
    
    Signed-off-by: David Scott <dave@recoil.org>

--- src/bin/logging.ml
+++ src/bin/logging.ml
@@ -10,13 +10,41 @@ open Cmdliner
 let mk = Arg.enum [
     "quiet"    , Quiet;
     "eventlog" , Eventlog;
-    "asl"      , ASL
+    "asl"      , ASL;
   ]
+
+  let pp_ptime f () =
+    let open Unix in
+    let tm = Unix.gmtime (Unix.time ()) in
+    Fmt.pf f "time=\"%04d-%02d-%02dT%02d:%02d:%02dZ\"" (tm.tm_year + 1900) (tm.tm_mon + 1)
+      tm.tm_mday tm.tm_hour tm.tm_min tm.tm_sec
+
+let reporter =
+  let report src level ~over k msgf =
+    let k _ =
+      over ();
+      k ()
+    in
+    let src = Logs.Src.name src in
+    let with_stamp _h _tags k fmt =
+      let level = Logs.level_to_string (Some level) in
+
+      Fmt.kpf k Fmt.stderr
+        ("\r%a level=%a @[msg=\"%a: " ^^ fmt ^^ "\"@]@.")
+        pp_ptime ()
+        Fmt.string level
+        Fmt.string src
+
+    in
+    msgf @@ fun ?header ?tags fmt ->
+    with_stamp header tags k fmt
+  in
+  { Logs.report }
 
 let setup log_destination level =
   Logs.set_level level;
   match log_destination with
-  | Quiet    -> Logs.set_reporter (Logs_fmt.reporter ())
+  | Quiet    -> Logs.set_reporter (reporter)
   | Eventlog ->
     let eventlog = Eventlog.register "Docker.exe" in
     Logs.set_reporter (Log_eventlog.reporter ~eventlog ())
