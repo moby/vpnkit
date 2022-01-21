bdddb8fa01089b23f7f6928ffd22c2337ced028e
David Scott <dave@recoil.org>
2022-01-21 16:43:09 +0000

    dune: generate COMMIT,vpnkit.tgz,licenses.json from OCaml
    Rather than use make and shell (redirection) which requires cygwin,
    generate the files from a cross-platform OCaml fragment.
    
    Signed-off-by: David Scott <dave@recoil.org>

--- scripts/appveyor.sh
+++ scripts/appveyor.sh
@@ -1,22 +1,10 @@
 #!/usr/bin/env sh
 
-echo Build is disabled.
-exit 0
-
-set -eu
+set -e
 
 cd "${APPVEYOR_BUILD_FOLDER}"
 
-export REPO_ROOT=$(git rev-parse --show-toplevel)
-export OPAM_REPO=$(cygpath.exe -w "${REPO_ROOT}/repo/win32")
-export OPAMROOT=$(cygpath.exe -w "${REPO_ROOT}/_build/opam")
-# NOTE: this is where all the binaries will end up, e.g. `oasis`
-# This must be on the end of the path or else Windows will try to
-# execute the opam metadata file in vpnkit!
-export PATH="${PATH}:${BINDIR}"
-
-make
-make test
-make artefacts
-make OSS-LICENSES
-make COMMIT
+opam exec -- dune build COMMIT
+opam exec -- dune build licenses.json
+opam exec -- dune build vpnkit.exe
+opam exec -- dune test
