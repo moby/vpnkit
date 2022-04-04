ce4c78645dbcd2a45201f7e9d2d646dfb6047ff7
David Scott <dave@recoil.org>
2022-04-04 14:57:05 +0100

    log: add process name, avoid splitting lines
    The @[ ... @] would do fancy line splitting and alignment, which we
    really don't want. We want to stick to one line per log record for
    easy parsing.
    
    Signed-off-by: David Scott <dave@recoil.org>

--- src/bin/logging.ml
+++ src/bin/logging.ml
@@ -8,6 +8,8 @@
     Fmt.pf f "time=\"%04d-%02d-%02dT%02d:%02d:%02d.%09dZ\"" (tm.tm_year + 1900) (tm.tm_mon + 1)
       tm.tm_mday tm.tm_hour tm.tm_min tm.tm_sec nsecs
 
+let process = Filename.basename Sys.argv.(0)
+
 let reporter =
   let report src level ~over k msgf =
     let k _ =
@@ -19,8 +21,9 @@ let reporter =
       let level = Logs.level_to_string (Some level) in
 
       Fmt.kpf k Fmt.stderr
-        ("\r%a level=%a @[msg=\"%a: " ^^ fmt ^^ "\"@]@.")
+        ("%a process=%a level=%a msg=\"%a: " ^^ fmt ^^ "\"@.")
         pp_ptime ()
+        Fmt.string process
         Fmt.string level
         Fmt.string src
 
