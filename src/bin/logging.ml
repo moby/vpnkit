c442adb2da27f430bba6b5d736ce529057660d29
David Scott <dave@recoil.org>
2022-11-08 21:50:45 +0000

    Revert "log: add process name, avoid splitting lines"
    This reverts commit ce4c78645dbcd2a45201f7e9d2d646dfb6047ff7.

--- src/bin/logging.ml
+++ src/bin/logging.ml
@@ -8,8 +8,6 @@
     Fmt.pf f "time=\"%04d-%02d-%02dT%02d:%02d:%02d.%09dZ\"" (tm.tm_year + 1900) (tm.tm_mon + 1)
       tm.tm_mday tm.tm_hour tm.tm_min tm.tm_sec nsecs
 
-let process = Filename.basename Sys.argv.(0)
-
 let reporter =
   let report src level ~over k msgf =
     let k _ =
@@ -21,9 +19,8 @@ let reporter =
       let level = Logs.level_to_string (Some level) in
 
       Fmt.kpf k Fmt.stderr
-        ("%a process=%a level=%a msg=\"%a: " ^^ fmt ^^ "\"@.")
+        ("\r%a level=%a @[msg=\"%a: " ^^ fmt ^^ "\"@]@.")
         pp_ptime ()
-        Fmt.string process
         Fmt.string level
         Fmt.string src
 
