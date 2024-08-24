fb65dd377e3650011674430ed4634831b39396e0
Anil Madhavapeddy <anil@recoil.org>
2024-08-24 12:24:37 +0100

    support dune 3.16.0

--- scripts/licenses.ml
+++ scripts/licenses.ml
@@ -547,7 +547,7 @@ src/arp_packet.ml mirage/arpv4.mli mirage/arpv4.ml
   | "jane-street-headers.v0.14.0"
   | "base.v0.14.2" | "base.v0.14.3"
   | "csexp.1.5.1"
-  | "dune.2.9.1" | "dune.2.9.2" | "dune.2.9.3" | "dune.3.0.2" | "dune.3.0.3" | "dune.3.1.1" | "dune.3.2.0" | "dune.3.3.0" | "dune.3.4.1" | "dune.3.5.0"
+  | "dune.2.9.1" | "dune.2.9.2" | "dune.2.9.3" | "dune.3.0.2" | "dune.3.0.3" | "dune.3.1.1" | "dune.3.2.0" | "dune.3.3.0" | "dune.3.4.1" | "dune.3.5.0" | "dune.3.16.0"
   | "dune-configurator.2.9.1" | "dune-configurator.2.9.3" | "dune-configurator.3.0.2" | "dune-configurator.3.0.3" | "dune-configurator.3.1.1" | "dune-configurator.3.3.0" | "dune-configurator.3.5.0"
   | "dune-configurator.3.2.0" | "dune-configurator.3.4.1"
   | "ocaml-compiler-libs.v0.12.4"
